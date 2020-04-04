071732525314f0287bd78aa09da61a5ba6dc1844
Diego Kunha <diegokunha@gmail.com>
2020-04-04 12:20:16 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/br/com/dksweb/speedFood/resources/CategoriaResource.java
+++ src/main/java/br/com/dksweb/speedFood/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package br.com.dksweb.speedFood.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dksweb.speedFood.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST está Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		
+		return lista;
 	}
 	
 }
