785a0e0fc3ddd60718ab89fd1e9a407425bca797
Diego Kunha <diegokunha@gmail.com>
2020-04-04 11:45:58 -0300

    Projeto criado

---
+++ src/main/java/br/com/dksweb/speedFood/SpeedFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.dksweb.speedFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dksweb/speedFood/SpeedFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dksweb.speedFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
