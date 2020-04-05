6d6474e4627035e0c675b0317237de6818d01726
Diego Kunha <diegokunha@gmail.com>
2020-04-05 20:36:26 -0300

    Ajustes no EndPoint /categorias/{id}

--- src/main/java/br/com/dksweb/speedFood/services/CategoriaService.java
+++ src/main/java/br/com/dksweb/speedFood/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.dksweb.speedFood.domain.Categoria;
 import br.com.dksweb.speedFood.repositories.CategoriaRepository;
+import br.com.dksweb.speedFood.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -19,7 +20,8 @@ public class CategoriaService {
 	 * */
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
